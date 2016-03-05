eca5b9492cabedc6e7b80153bed5bf0c83e981e5
segupe1 <segupe1@gmail.com>
2016-03-05 20:24:37 +0100

    problem backend viewed fixed

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -6,11 +6,28 @@ use Illuminate\Http\Request;
 use listong\Http\Requests;
 use listong\Http\Controllers\Controller;
 use listong\Ong;
+use Gmaps;
 
 class BackController extends Controller
 {
     function index(){
     	$ong = Ong::paginate ( 5 );
-    	return view ( 'admin.index', compact ( 'ong' ) );
+    	//configuaración
+        $config = array();
+        $config['center'] = '37.4419, -122.1419';
+        $config['map_width'] = 700;
+        $config['map_height'] = 500;
+        //$config['onclick'] = 'alert(\'You just clicked at: \' + event.latLng.lat() + \', \' + event.latLng.lng()); createMarker_map({ map: map, position:event.latLng });';
+        $config['onclick'] = 'fill_Location(event.latLng.lat(), event.latLng.lng());createMarker_map({ map: map, position:event.latLng });';
+        $config['zoom'] = 'auto';
+        
+        Gmaps::initialize($config);
+ 
+        $marker = array();
+    
+        Gmaps::add_marker($marker);
+ 
+        $map = Gmaps::create_map();
+    	return view ( 'admin.index', compact ( 'ong','map' ) );
     }
 }
